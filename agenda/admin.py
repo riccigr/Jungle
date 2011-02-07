db1f7a598963b3d11d6cfb73d21ccb9dc7be6605
Ricci <Ricci@Mordor.local>
2011-02-07 13:54:31 -0200

    2011-02-07 inserida tela de admin

---
+++ agenda/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from Jungle.agenda.models import ItemAgenda
+
+class ItemAgendaAdmin(admin.ModelAdmin):
+	fields = ('data', 'hora', 'titulo', 'descricao', 'participantes')
+	list_display = ('data', 'hora', 'titulo')
+	
+	def queryset(self, request):
+		qs = super(ItemAgendaAdmin, self).queryset(request)
+		return qs.filter(usuario=request.user)
+		
+	def save_model(self, request, obj, form, change):
+		obj.usuario = request.user
+		obj.save()	
+		
+admin.site.register(ItemAgenda, ItemAgendaAdmin)		
