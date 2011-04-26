ab5a6edb8732416ac4bfcf63911650639009dda9
Guilherme Ricci Gaiofatto <ricci.gr@gmail.com>
2011-04-26 12:35:44 -0300

    invalidos

--- agenda/models.py
+++ agenda/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 class ItemAgenda(models.Model):
 	data = models.DateField()
@@ -8,3 +9,24 @@ class ItemAgenda(models.Model):
 	descricao = models.TextField()
 	usuario = models.ForeignKey(User, related_name='item_usuario')
 	participantes = models.ManyToManyField(User, related_name='item_participantes')
+'''
+def envia_email(**kwargs):
+	print "Essa function enviara emails, aguarde..."
+	try:
+		item = kwargs['instance']
+	except KeyError:
+		print "ops"
+		return
+	for part in item.participantes.all():
+		print "tem paricipantes"
+		if part.email:
+			print "aki foi"
+			dados = (item.titulo, 
+					datetime.strftime(item.data, "%d/%m/%Y"), 
+					item.hora)
+			part.email_user(subject = "[evento] %s dia %s as %s" % dados,
+			 				message = "Evento: %s \nDia: %s \nHora: %s" % dados,
+							from_email = item.usuario.email)	
+	
+models.signals.post_save.connect(envia_email, sender=ItemAgenda, dispatch_uid="agenda.models.ItemAgenda")
+'''
